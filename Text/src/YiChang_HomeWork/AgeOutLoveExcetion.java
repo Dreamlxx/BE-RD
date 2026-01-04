521a2cc3c0dfa73c9796ce5c1de0d19fcd0ae046
Dreamlx <1773632641@qq.com>
2026-01-04 19:18:06 +0800

    15522

---
+++ Text/src/YiChang_HomeWork/AgeOutLoveExcetion.java
@@ -0,0 +1,10 @@
+package YiChang_HomeWork;
+
+public class AgeOutLoveExcetion extends RuntimeException{
+    public AgeOutLoveExcetion() {
+    }
+
+    public AgeOutLoveExcetion(String message) {
+        super(message);
+    }
+}
---
+++ Text/src/YiChang_HomeWork/NameFormatExcetion.java
@@ -0,0 +1,10 @@
+package YiChang_HomeWork;
+
+public class NameFormatExcetion extends RuntimeException{
+    public NameFormatExcetion(String message) {
+        super(message);
+    }
+
+    public NameFormatExcetion() {
+    }
+}
